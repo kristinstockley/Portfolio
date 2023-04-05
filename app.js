7898850644b1309d67b91d6908fe6bc2cfd9a67d
Kristin Stockley <kbstockley@gmail.com>
2023-04-05 16:41:53 -0500

    4/5 Commit

--- app.js
+++ app.js
@@ -5,52 +5,75 @@ function openInNewWindow(event) {
 }
 
 
+
 // GRADUATION COUNTDOWN
+const startButton = document.getElementById('countdownButton');
 
-const countDownDate = new Date("Jun 12, 2023 00:00:00").getTime();
+startButton.addEventListener('click', function() {
+  const countDownDate = new Date("Jun 12, 2023 00:00:00").getTime();
+  let x = 0;
 
-const x = setInterval(function() {
-
-const now = new Date().getTime();
-
-const distance = countDownDate - now;
-
-const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-if (distance < 0) {
+  function startCountdown() {
+    x = setInterval(function() {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      document.getElementById("countdownButton").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
-    }
-}, 1000);
+        document.getElementById("countdownButton").innerHTML = "GRADUATED";
+      }
+    }, 1000);
+  }
+
+  startCountdown();
+});
 
 
 // DARK MODE
-const darkModeButton = document.querySelector('#dark-mode-button');
-darkModeButton.addEventListener('click', toggleDarkMode);
-  
-function toggleDarkMode() {
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-mode');
+const darkModeButton = document.getElementById('darkMode');
+
+darkModeButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
+
+
+// RESUME MODAL
+function openResume() {
+    const resumeModal = document.querySelector('#resumeModal');
+    const resumeIframe = document.querySelector('#resumeIframe');
+    resumeIframe.src = './Kristin Resume.pdf';
+    const modal = new bootstrap.Modal(resumeModal);
+    modal.show();
 }
 
 
 
-// FORM SUBMISSION IN PROGRESS
-const form = document.getElementById('contact-form');
+// SCROLL TO TOP
+const scrollBtn = document.getElementById("scrollBtn");
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  
-  const formData = new FormData(form);
-  
-  const request = new XMLHttpRequest();
-  request.open('POST', '/submit-contact-form');
-  request.send(formData);
-  
-  alert('Thanks for your message!');
-});
+window.onscroll = function() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.onclick = function() {
+  scrollToTop();
+};
+
+function scrollToTop() {
+  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    setTimeout(scrollToTop, 1000);
+  }
+}
