46590e93380eae436f6808998e56a80d568b3404
Kristin Stockley <kbstockley@gmail.com>
2023-06-18 18:17:43 -0500

    updated styling and projects

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Portfolio
 
-This is my (currently template) portfolio to showcase my skills and future projects. You can view the live site at https://kristinstockley.netlify.app/.
+This is my portfolio to showcase my skills and projects. You can view the live site at https://kristinstockley.netlify.app/.
 
 <h2>Technologies Used:</h2>
 <ul>
@@ -10,12 +10,3 @@ This is my (currently template) portfolio to showcase my skills and future proje
 <li>Bootstrap</li>
 <li>Netlify (for deployment)</li>
 </ul>
-
-<h2>Approach Taken</h2>
-My goal was to keep the portfolio simple and clean, with easy navigation. I started with Bootstrap and custom CSS to create a responsive and visually appealing design. Once I had the layout down, I used JavaScript for user interations including my graduation countdown, the dark/light mode toggle, option to scroll to the top, and carousel.
-
-
-<h2>Unsolved Problems:</h2>
-<li>Project card backgrounds changing with Dark/Light Mode</li>
-<li>Resume link in navbar staying black after user click</li>
-<li>Resume showing 2nd page on mobile</li>
--- app.js
+++ app.js
@@ -1,44 +1,8 @@
-// GRADUATION COUNTDOWN
-const startButton = document.getElementById('countdownButton');
-
-startButton.addEventListener('click', function() {
-  const countDownDate = new Date("Jun 12, 2023 00:00:00").getTime();
-  let x = 0;
-
-  function startCountdown() {
-    x = setInterval(function() {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      document.getElementById("countdownButton").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdownButton").innerHTML = "GRADUATED";
-      }
-    }, 1000);
-  }
-
-  startCountdown();
-});
-
-
-// DARK MODE
-const darkModeButton = document.getElementById('darkMode');
-
-darkModeButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
-
-
-
 // RESUME MODAL
 function openResume() {
     const resumeModal = document.querySelector('#resumeModal');
     const resumeIframe = document.querySelector('#resumeIframe');
-    resumeIframe.src = './Kristin Resume.pdf';
+    resumeIframe.src = './Kristin Stockley Resume.pdf';
     const modal = new bootstrap.Modal(resumeModal);
     modal.show();
 }
@@ -69,47 +33,3 @@ function scrollToTop() {
     setTimeout(scrollToTop, 1000);
   }
 }
-
-// CAROUSEL WORK IN PROGRESS
-const carousel = document.querySelector('.carousel');
-const container = carousel.querySelector('.carouselContainer');
-const prevBtn = carousel.querySelector('.carouselPrev');
-const nextBtn = carousel.querySelector('.carouselNext');
-
-let currentSlide = 0;
-let intervalId;
-
-function startInterval() {
-  intervalId = setInterval(() => {
-    currentSlide++;
-    if (currentSlide === container.children.length) {
-      currentSlide = 0;
-    }
-    container.style.transform = `translateX(-${currentSlide * 600}px)`;
-  }, 2000);
-}
-
-function stopInterval() {
-  clearInterval(intervalId);
-}
-
-function onPrevClick() {
-  stopInterval();
-  currentSlide--;
-  if (currentSlide < 0) {
-    currentSlide = container.children.length - 1;
-  }
-  container.style.transform = `translateX(-${currentSlide * 600}px)`;
-}
-
-function onNextClick() {
-  stopInterval();
-  currentSlide++;
-  if (currentSlide === container.children.length) {
-    currentSlide = 0;
-  }
-  container.style.transform = `translateX(-${currentSlide * 600}px)`;
-}
-
-prevBtn.addEventListener('click', onPrevClick);
-nextBtn.addEventListener('click', onNextClick);
