e3f0452a6555bc00df70fffb1706f212c002737d
Kristin Stockley <kbstockley@gmail.com>
2023-09-15 13:32:12 -0500

    media queries and added animation

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
-// SCROLL TO TOP
-const scrollBtn = document.getElementById("scrollBtn");
+// Typing text
+const text = "Hi, I'm Kristin.";
+const typingSpeed = 100;
 
-window.onscroll = function() {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    scrollBtn.style.display = "block";
-  } else {
-    scrollBtn.style.display = "none";
-  }
+const typingTextElement = document.getElementById('typing-text');
+typingTextElement.classList.add('blinking-cursor');
+
+function typeText() {
+	let i = 0;
+	const typingInterval = setInterval(function () {
+		if (i < text.length) {
+			typingTextElement.textContent += text.charAt(i);
+			i++;
+		} else {
+			clearInterval(typingInterval);
+		}
+	}, typingSpeed);
+}
+
+window.addEventListener('load', () => {
+	typeText();
+});
+
+// SCROLL TO TOP
+const scrollBtn = document.getElementById('scrollBtn');
+
+window.onscroll = function () {
+	if (
+		document.body.scrollTop > 700 ||
+		document.documentElement.scrollTop > 700
+	) {
+		scrollBtn.style.display = 'block';
+	} else {
+		scrollBtn.style.display = 'none';
+	}
 };
 
-scrollBtn.onclick = function() {
-  scrollToTop();
+scrollBtn.onclick = function () {
+	scrollToTop();
 };
 
 function scrollToTop() {
-  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-    setTimeout(scrollToTop, 1000);
-  }
+	if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+		setTimeout(scrollToTop, 1000);
+	}
 }
